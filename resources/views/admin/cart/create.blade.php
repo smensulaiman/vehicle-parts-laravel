a1064f20f0646a31c0435ca56382e1869adff2c4
Rahat-Minhaj007 <rahatminhaj444@gmail.com>
2024-10-31 16:52:25 +0900

    cart page added

--- resources/views/admin/cart/create.blade.php
+++ resources/views/admin/cart/create.blade.php
@@ -1,15 +1,195 @@
 @extends('admin.layouts.master')
 
 @section('content')
-
     <section class="content-main">
         <div class="content-header">
             <h2 class="content-title">Cart</h2>
         </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card " style="min-width: 18rem;">
+                    <div class="card-body">
+                        <div class="row gx-5">
+                            <!-- Table Part 1 -->
+                            <div class="col-xxl-6 col-xl-12 col-sm-12">
+                                <h4 class="d-flex align-items-center justify-content-center">
+                                    <span>
+                                        <i class="fas fa-list"></i>
+                                    </span>
+                                    <span class="ms-2">Items List</span>
+                                </h4>
 
-        <p>Hello world</p>
-{{--        @livewire('cart-items')--}}
-        <p>This is a conflict test</p>
+                                <div class="table-responsive pt-3">
+                                    <table class="table table-bordered table-hover">
+                                        <thead class="table-light">
+                                            <tr class="text-center">
+                                                <th scope="col">#SN</th>
+                                                <th scope="col">Item</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Quantity</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="text-center">
+                                            <tr>
+                                                <th scope="row"><a href="#" class="fw-bold">#1</a></th>
+                                                <td>Mark</td>
+                                                <td>¥299.99</td>
+                                                <td>700</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-md rounded font-sm"><i
+                                                            class="fas fa-add"></i></a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row"><a href="#" class="fw-bold">#2</a></th>
+                                                <td>Jacob</td>
+                                                <td>¥299.99</td>
+                                                <td>400</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-md rounded font-sm"><i
+                                                            class="fas fa-add"></i></a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row"><a href="#" class="fw-bold">#3</a></th>
+                                                <td>Larry the Bird</td>
+                                                <td>¥299.99</td>
+                                                <td>300</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-md rounded font-sm"><i
+                                                            class="fas fa-add"></i></a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                </div>
+                            </div>
+
+                            <!-- Checkout Part 2 -->
+                            <div class="col-xxl-6 col-xl-12 col-sm-12">
+                                <h4 class="d-flex align-items-center justify-content-center">
+                                    <span>
+                                        <i class="fas fa-shopping-cart"></i>
+                                    </span>
+                                    <span class="ms-2">Your Cart (2 items)</span>
+                                </h4>
+                                <!-- Cart Content -->
+                                <div class="table-responsive pt-3">
+
+                                    <table class="table table-bordered table-hover">
+                                        <thead class="table-light">
+                                            <tr class="border-bottom text-center">
+                                                <th scope="col">Item</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Quantity</th>
+                                                <th scope="col">Total</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="text-center">
+                                            <tr class="border-bottom">
+                                                <td class="d-flex justify-items-center align-items-center gap-3 p-3">
+                                                    <div>
+                                                        <img style="height:4rem; width:4rem; object-fit: contain;"
+                                                            src="../../assets/imgs/car-parts/tier.jpg" alt="Product 1"
+                                                            class="img-fluid">
+                                                    </div>
+                                                    <div>
+                                                        <p>Product Name</p>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <p>¥299.99</p>
+                                                </td>
+                                                <td>
+                                                    <div class="input-group d-flex justify-content-center ">
+                                                        <div class="btn btn-light btn-sm">+</div>
+                                                        <input type="number" class="form-control text-center w-25"
+                                                            value="1">
+                                                        <div class="btn btn-light btn-sm">-</div>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <p>¥299.99</p>
+                                                </td>
+                                                <td>
+                                                    <a href="#" class="btn btn-md rounded font-sm"><i
+                                                            class="fas fa-trash"></i></a>
+                                                </td>
+                                            </tr>
+                                            <tr class="border-bottom">
+                                                <td
+                                                    class="d-flex justify-items-center align-items-center gap-3 p-3 text-center">
+                                                    <div>
+                                                        <img style="height:4rem; width:4rem; object-fit: contain;"
+                                                            src="../../assets/imgs/car-parts/steering.jpg" alt="Product 2"
+                                                            class="img-fluid">
+                                                    </div>
+                                                    <div>
+                                                        <p>Product Name</p>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <p>¥100.00</p>
+                                                </td>
+                                                <td class="text-center">
+                                                    <div class="input-group">
+                                                        <div class="btn btn-light btn-sm">+</div>
+                                                        <input type="number" class="form-control text-center w-25"
+                                                            value="1">
+                                                        <div class="btn btn-light btn-sm">-</div>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <p>¥100.00</p>
+                                                </td>
+                                                <td class="text-center">
+                                                    <a href="#" class="btn btn-md rounded font-sm"> <i
+                                                            class="fas fa-trash"></i></a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+
+                                    </table>
+
+                                </div>
+
+                                <!-- Lower Part Of checkout -->
+                                <div class="row px-2 d-flex justify-content-end">
+                                    <div class="col-md-6">
+                                        <div class="row  border-bottom py-2">
+                                            <h6 class="col-sm-9 col-8">Subtotal:</h6>
+                                            <p class="col-sm-3 col-4">¥ 399.00</p>
+                                        </div>
+                                        <div class="row border-bottom py-2">
+                                            <h6 class="col-sm-9 col-8">Tax:</h6>
+                                            <p class="col-sm-3 col-4">¥ 100.99</p>
+                                        </div>
+                                        <div class="row py-2">
+                                            <h6 class="col-sm-9 col-8">Grand Total:</h6>
+                                            <p class="col-sm-3 col-4">¥ 499.99</p>
+                                        </div>
+                                        <div class="row py-2">
+                                            <button
+                                                class="btn btn-md rounded font-sm text-center d-flex align-items-center justify-content-center fw-bold">
+                                                <i class="fas fa-shopping-cart"></i>
+                                                <span class="ms-2">CHECKOUT</span>
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </section>
-
 @endsection
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,60 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <title>Parts Dashboard</title>
-    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
-    <meta name="description" content=""/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/imgs/theme/favicon.svg')}}"/>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/imgs/theme/favicon.svg') }}" />
     <!-- Template CSS -->
-    <script src="{{asset('assets/js/vendors/color-modes.js')}}"></script>
-    <link href="{{asset('assets/css/main.css?v=6.0')}}" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('assets/css/vendors/datatables.bootstrap.css')}}" type="text/css">
-    <link rel="stylesheet" href="{{asset('assets/css/custom.css')}}">
+    <script src="{{ asset('assets/js/vendors/color-modes.js') }}"></script>
+    <link href="{{ asset('assets/css/main.css?v=6.0') }}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('assets/css/vendors/datatables.bootstrap.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+
+    {{-- Font Awesome Icon --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     @livewireStyles
 </head>
 
 <body>
-<div class="screen-overlay"></div>
+    <div class="screen-overlay"></div>
 
-@include('admin.layouts.sidebar')
+    @include('admin.layouts.sidebar')
 
-<main class="main-wrap">
-    <header class="main-header navbar">
-        @include('admin.layouts.navbar')
-    </header>
-    @yield('content')
-    <!-- content-main end// -->
-    <footer class="main-footer font-xs">
-        <div class="row pt-15">
-            <div class="col-sm-6">
-                <script>
-                    document.write(new Date().getFullYear());
-                </script>
-                &copy; Parts Management System - Senda Japan Ltd.
+    <main class="main-wrap">
+        <header class="main-header navbar">
+            @include('admin.layouts.navbar')
+        </header>
+        @yield('content')
+        <!-- content-main end// -->
+        <footer class="main-footer font-xs">
+            <div class="row pt-15">
+                <div class="col-sm-6">
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+                    &copy; Parts Management System - Senda Japan Ltd.
+                </div>
             </div>
-        </div>
-    </footer>
-</main>
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-{{--<script src="{{asset('assets/js/vendors/bootstrap.bundle.min.js')}}"></script>--}}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-<script src="{{asset('assets/js/vendors/select2.min.js')}}"></script>
-<script src="{{asset('assets/js/vendors/perfect-scrollbar.js')}}"></script>
-<script src="{{asset('assets/js/vendors/jquery.fullscreen.min.js')}}"></script>
-<script src="{{asset('assets/js/vendors/chart.js')}}"></script>
-<!-- Main Script -->
-<script src="{{asset('assets/js/main.js?v=6.0')}}" type="text/javascript"></script>
-<script src="{{asset('assets/js/custom-chart.js')}}" type="text/javascript"></script>
-<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
-<script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+        </footer>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    {{-- <script src="{{asset('assets/js/vendors/bootstrap.bundle.min.js')}}"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('assets/js/vendors/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendors/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('assets/js/vendors/jquery.fullscreen.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendors/chart.js') }}"></script>
+    <!-- Main Script -->
+    <script src="{{ asset('assets/js/main.js?v=6.0') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/js/custom-chart.js') }}" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
 
-@stack('scripts')
-@livewireScripts
+    @stack('scripts')
+    @livewireScripts
 
 </body>
+
 </html>
