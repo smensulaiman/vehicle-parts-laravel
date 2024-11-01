a4341acaeeff808804ec80f7330bb1143f0ad8b1
Rahat-Minhaj007 <rahatminhaj444@gmail.com>
2024-11-01 15:58:02 +0900

    brushup on cart page and change the layout

--- resources/views/admin/cart/create.blade.php
+++ resources/views/admin/cart/create.blade.php
@@ -6,21 +6,34 @@
             <h2 class="content-title">Cart</h2>
         </div>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-xxl-2">
                 <div class="card">
                     <div class="card-body">
-                        <div class="row gx-5">
+                        <div class="row">
                             <!-- Table Part 1 -->
                             <div class="col-xxl-12 col-xl-12 col-sm-12">
-                                <h4 class="d-flex align-items-center justify-content-center">
-                                    <span>
-                                        <i class="fas fa-list"></i>
-                                    </span>
-                                    <span class="ms-2">Items List</span>
-                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-5">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <!-- Table Part 1 -->
+                            <div class="col-xxl-12 col-xl-12 col-sm-12">
+                                <header>
+                                    <h4 class="d-flex align-items-center justify-content-center">
+                                        <span>
+                                            <i class="fas fa-list"></i>
+                                        </span>
+                                        <span class="ms-2">Items List</span>
+                                    </h4>
+                                </header>
 
                                 <div class="table-responsive pt-3">
-                                    <table class="table table-bordered table-hover">
+                                    <table class="table table-hover">
                                         <thead class="table-light">
                                             <tr class="text-center">
                                                 <th scope="col">#SN</th>
@@ -32,7 +45,7 @@
                                         </thead>
                                         <tbody class="text-center">
                                             <tr>
-                                                <th scope="row"><a href="#" class="fw-bold">#1</a></th>
+                                                <td scope="row"><a href="#" class="fw-bold">#1</a></td>
                                                 <td>Mark</td>
                                                 <td>¥299.99</td>
                                                 <td>700</td>
@@ -42,7 +55,7 @@
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <th scope="row"><a href="#" class="fw-bold">#2</a></th>
+                                                <td scope="row"><a href="#" class="fw-bold">#2</a></td>
                                                 <td>Jacob</td>
                                                 <td>¥299.99</td>
                                                 <td>400</td>
@@ -52,7 +65,7 @@
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <th scope="row"><a href="#" class="fw-bold">#3</a></th>
+                                                <td scope="row"><a href="#" class="fw-bold">#3</a></td>
                                                 <td>Larry the Bird</td>
                                                 <td>¥299.99</td>
                                                 <td>300</td>
@@ -72,26 +85,28 @@
                 </div>
             </div>
 
-            <div class="col-lg-6">
+            <div class="col-xxl-5">
                 <div class="card">
                     <div class="card-body">
-                        <div class="row gx-5">
+                        <div class="row">
                             <!-- Checkout Part 2 -->
                             <div class="col-xxl-12 col-xl-12 col-sm-12">
-                                <h4 class="d-flex align-items-center justify-content-center">
-                                    <span>
-                                        <i class="fas fa-shopping-cart"></i>
-                                    </span>
-                                    <span class="ms-2">Your Cart (2 items)</span>
-                                </h4>
+                                <header>
+                                    <h4 class="d-flex align-items-center justify-content-center">
+                                        <span>
+                                            <i class="fas fa-shopping-cart"></i>
+                                        </span>
+                                        <span class="ms-2">Your cart (2 items)</span>
+                                    </h4>
+                                </header>
                                 <!-- Cart Content -->
                                 <div class="table-responsive pt-3">
 
-                                    <table class="table table-bordered table-hover">
+                                    <table class="table">
                                         <thead class="table-light">
                                             <tr class="border-bottom text-center">
+                                                <th scope="col">#SN</th>
                                                 <th scope="col">Item</th>
-                                                <th scope="col">Price</th>
                                                 <th scope="col">Quantity</th>
                                                 <th scope="col">Total</th>
                                                 <th scope="col">Action</th>
@@ -99,26 +114,24 @@
                                         </thead>
                                         <tbody class="text-center">
                                             <tr class="border-bottom">
-                                                <td class="d-flex justify-items-center align-items-center gap-3 p-3">
+                                                <td scope="row"><a href="#" class="fw-bold">#1</a></td>
+                                                <td class="d-flex justify-content-center align-items-center gap-3 p-3">
                                                     <div>
                                                         <img style="height:4rem; width:4rem; object-fit: contain;"
                                                             src="../../assets/imgs/car-parts/tier.jpg" alt="Product 1"
                                                             class="img-fluid">
                                                     </div>
                                                     <div>
-                                                        <p>Product Name</p>
+                                                        <p>Steering</p>
+                                                        <p>¥299.99</p>
                                                     </div>
                                                 </td>
                                                 <td>
-                                                    <p>¥299.99</p>
-                                                </td>
-                                                <td>
-                                                    <div class="input-group d-flex justify-content-center ">
-
-                                                        <div class="btn btn-light btn-sm">-</div>
+                                                    <div class="input-group d-flex justify-content-center">
+                                                        <a href="#" class="btn btn-light btn-sm fw-bold">-</a>
                                                         <input type="number" class="form-control text-center"
                                                             value="1" style="width:4rem;">
-                                                        <div class="btn btn-light btn-sm">+</div>
+                                                        <a href="#" class="btn btn-light btn-sm fw-bold">+</a>
                                                     </div>
                                                 </td>
                                                 <td class="text-center">
@@ -130,27 +143,25 @@
                                                 </td>
                                             </tr>
                                             <tr class="border-bottom">
+                                                <td scope="row"><a href="#" class="fw-bold">#2</a></td>
                                                 <td
-                                                    class="d-flex justify-items-center align-items-center gap-3 p-3 text-center">
+                                                    class="d-flex justify-content-center align-items-center gap-3 p-3 text-center">
                                                     <div>
                                                         <img style="height:4rem; width:4rem; object-fit: contain;"
                                                             src="../../assets/imgs/car-parts/steering.jpg" alt="Product 2"
                                                             class="img-fluid">
                                                     </div>
                                                     <div>
-                                                        <p>Product Name</p>
+                                                        <p>Tier</p>
+                                                        <p>¥100.00</p>
                                                     </div>
                                                 </td>
                                                 <td class="text-center">
-                                                    <p>¥100.00</p>
-                                                </td>
-                                                <td class="text-center">
                                                     <div class="input-group">
-
-                                                        <div class="btn btn-light btn-sm">-</div>
+                                                        <a href="#" class="btn btn-light btn-sm fw-bold">-</a>
                                                         <input type="number" class="form-control text-center"
                                                             value="1" style="width:4rem;">
-                                                        <div class="btn btn-light btn-sm">+</div>
+                                                        <a href="#" class="btn btn-light btn-sm fw-bold">+</a>
                                                     </div>
                                                 </td>
                                                 <td class="text-center">
@@ -168,8 +179,8 @@
                                 </div>
 
                                 <!-- Lower Part Of checkout -->
-                                <div class="row px-2 d-flex justify-content-end">
-                                    <div class="col-md-6">
+                                <div class="row px-3 d-flex justify-content-end">
+                                    <div class="col-lg-8">
                                         <div class="row  border-bottom py-2">
                                             <h6 class="col-sm-9 col-8">Subtotal:</h6>
                                             <p class="col-sm-3 col-4">¥ 399.00</p>
--- resources/views/admin/parts/category/index.blade.php
+++ resources/views/admin/parts/category/index.blade.php
@@ -16,7 +16,8 @@
                     <header class="card-header">
                         <div class="d-flex justify-content-between align-items-center">
                             <h6 class="card-title">Total Vehicles</h6>
-                            <a href="{{ route('admin.part-category.create') }}" class="btn btn-primary btn-sm rounded">Create New</a>
+                            <a href="{{ route('admin.part-category.create') }}" class="btn btn-primary btn-sm rounded">Create
+                                New</a>
                         </div>
                     </header>
                     <div class="card-body">
@@ -24,46 +25,47 @@
                             <div class="table-responsive">
                                 <table class="table table-bordered table-nowrap mb-0" style="table-layout: auto">
                                     <thead class="table-light border-1">
-                                    <tr>
-                                        <th class="align-middle" scope="col" width="60">S/N</th>
-                                        <th class="align-middle" scope="col">Part Name</th>
-                                        <th class="align-middle text-center" scope="col">Quantity</th>
-                                        <th class="align-middle text-end" scope="col">Price</th>
-                                        <th class="align-middle" scope="col">Generic</th>
-                                        <th class="align-middle text-center" scope="col">Action</th>
-                                    </tr>
+                                        <tr>
+                                            <th class="align-middle" scope="col" width="60">S/N</th>
+                                            <th class="align-middle" scope="col">Part Name</th>
+                                            <th class="align-middle text-center" scope="col">Quantity</th>
+                                            <th class="align-middle text-end" scope="col">Price</th>
+                                            <th class="align-middle" scope="col">Generic</th>
+                                            <th class="align-middle text-center" scope="col">Action</th>
+                                        </tr>
                                     </thead>
                                     <tbody>
 
-                                    @if(!empty($partNames))
-                                        @foreach($partNames as $partName)
-                                            <tr>
-                                                <td class="border-1 text-center">
-                                                    <a href="#" class="fw-bold">#{{$partName->id}}</a>
-                                                </td>
-                                                <td class="border-1">{{$partName->name}}</td>
-                                                <td class="text-center border-1">{{$partName->quantity}}</td>
-                                                <td class="text-end border-1">{{$partName->price}}</td>
-                                                <td class="border-1 text-center">{{$partName->is_generic ? 'yes' : 'no'}}</td>
-                                                <td class="border-1 px-0">
-                                                    <div class="d-flex justify-content-evenly text-end">
-                                                        <a href="#" class="btn btn-sm font-sm rounded btn-brand">
-                                                            <i class="material-icons md-edit"></i>
-                                                            <span>Edit</span>
-                                                        </a>
-                                                        <a href="#" class="btn btn-sm font-sm btn-light rounded">
-                                                            <i class="material-icons md-delete_forever"></i>
-                                                            <span>Delete</span>
-                                                        </a>
-                                                    </div>
-                                                </td>
+                                        @if (!empty($partNames))
+                                            @foreach ($partNames as $partName)
+                                                <tr>
+                                                    <td class="border-1 text-center">
+                                                        <a href="#" class="fw-bold">#{{ $partName->id }}</a>
+                                                    </td>
+                                                    <td class="border-1">{{ $partName->name }}</td>
+                                                    <td class="text-center border-1">{{ $partName->quantity }}</td>
+                                                    <td class="text-end border-1">{{ $partName->price }}</td>
+                                                    <td class="border-1 text-center">
+                                                        {{ $partName->is_generic ? 'yes' : 'no' }}</td>
+                                                    <td class="border-1 px-0">
+                                                        <div class="d-flex justify-content-evenly text-end">
+                                                            <a href="#" class="btn btn-sm font-sm rounded btn-brand">
+                                                                <i class="material-icons md-edit"></i>
+                                                                <span>Edit</span>
+                                                            </a>
+                                                            <a href="#" class="btn btn-sm font-sm btn-light rounded">
+                                                                <i class="material-icons md-delete_forever"></i>
+                                                                <span>Delete</span>
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        @else
+                                            <tr class="text-center">
+                                                <td class="text-danger" colspan="5">No Data</td>
                                             </tr>
-                                        @endforeach
-                                    @else
-                                        <tr class="text-center">
-                                            <td class="text-danger" colspan="5">No Data</td>
-                                        </tr>
-                                    @endif
+                                        @endif
                                     </tbody>
                                 </table>
                             </div>
