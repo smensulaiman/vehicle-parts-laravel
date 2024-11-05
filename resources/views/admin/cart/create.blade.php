c97c93cdf874f69199db5393e1f5fe71762821e5
Rahat-Minhaj007 <rahatminhaj444@gmail.com>
2024-11-05 12:50:16 +0900

    brushup on filter layout

--- resources/views/admin/cart/create.blade.php
+++ resources/views/admin/cart/create.blade.php
@@ -5,52 +5,54 @@
         <div class="content-header">
             <h2 class="content-title">Cart</h2>
         </div>
+
+        {{-- Filtering part --}}
         <div class="row">
-            <div class="col-xxl-2">
+            <div class="col-xxl-12">
                 <div class="card">
                     <div class="card-body">
-                        <div class="row">
+                        <div class="row g-3">
                             <!-- Table Part 1 -->
                             <div class="col-xxl-12 col-xl-12 col-sm-12">
                                 <header>
                                     <h4 class="d-flex align-items-center justify-content-center">
                                         <span>
-                                            <i class="fas fa-filters"></i>
+                                            <i class="fas fa-filter"></i>
                                         </span>
                                         <span class="ms-2">Filters</span>
                                     </h4>
                                 </header>
                                 <div class="pt-3 row">
-                                    <div class="col-lg-12">
+                                    <div class="col-lg-4">
                                         <label for="make" class="pb-1 ps-1 fw-bold text-primary">
                                             <span>Make</span>
                                         </label>
                                         <select class="form-select p-2" multiple aria-label="multiple select example"
-                                                name="make" id="make" style="height: 7rem;overflow-y: scroll;">
+                                            name="make" id="make" style="height: 7rem;overflow-y: scroll;">
                                             <option class="p-2" value="">Select Menu</option>
                                             <option class="p-2" value="1">One</option>
                                             <option class="p-2" value="2">Two</option>
                                             <option class="p-2" value="3">Three</option>
                                         </select>
                                     </div>
-                                    <div class="col-lg-12 pt-2">
+                                    <div class="col-lg-4">
                                         <label for="make" class="pb-1 ps-1 fw-bold text-primary">
                                             <span>Model</span>
                                         </label>
                                         <select class="form-select p-2" multiple aria-label="multiple select example"
-                                                name="make" id="make" style="height: 7rem;overflow-y: scroll;">
+                                            name="make" id="make" style="height: 7rem;overflow-y: scroll;">
                                             <option class="p-2" value="">Select Menu</option>
                                             <option class="p-2" value="1">One</option>
                                             <option class="p-2" value="2">Two</option>
                                             <option class="p-2" value="3">Three</option>
                                         </select>
                                     </div>
-                                    <div class="col-lg-12 pt-2">
+                                    <div class="col-lg-4">
                                         <label for="make" class="pb-1 ps-1 fw-bold text-primary">
                                             <span>Model Grade</span>
                                         </label>
                                         <select class="form-select p-2" multiple aria-label="multiple select example"
-                                                name="make" id="make" style="height: 7rem;overflow-y: scroll;">
+                                            name="make" id="make" style="height: 7rem;overflow-y: scroll;">
                                             <option class="p-2" value="">Select Menu</option>
                                             <option class="p-2" value="1">One</option>
                                             <option class="p-2" value="2">Two</option>
@@ -63,11 +65,16 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xxl-5">
+        </div>
+
+        {{-- Cart & Items List Part --}}
+        <div class="row">
+            <!-- Table Part 1 For Items List -->
+            <div class="col-xxl-6">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
-                            <!-- Table Part 1 -->
+
                             <div class="col-xxl-12 col-xl-12 col-sm-12">
                                 <header>
                                     <h4 class="d-flex align-items-center justify-content-center">
@@ -81,45 +88,45 @@
                                 <div class="table-responsive pt-3">
                                     <table class="table table-hover">
                                         <thead class="table-light">
-                                        <tr class="text-center">
-                                            <th scope="col">#SN</th>
-                                            <th scope="col">Item</th>
-                                            <th scope="col">Price</th>
-                                            <th scope="col">Quantity</th>
-                                            <th scope="col">Action</th>
-                                        </tr>
+                                            <tr class="text-center">
+                                                <th scope="col">#SN</th>
+                                                <th scope="col">Item</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Quantity</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
                                         </thead>
                                         <tbody class="text-center">
-                                        <tr>
-                                            <td scope="row"><a href="#" class="fw-bold">#1</a></td>
-                                            <td>Mark</td>
-                                            <td>¥299.99</td>
-                                            <td>700</td>
-                                            <td>
-                                                <a href="#" class="btn btn-md rounded font-sm"><i
-                                                        class="fas fa-shopping-cart"></i></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td scope="row"><a href="#" class="fw-bold">#2</a></td>
-                                            <td>Jacob</td>
-                                            <td>¥299.99</td>
-                                            <td>400</td>
-                                            <td>
-                                                <a href="#" class="btn btn-md rounded font-sm"><i
-                                                        class="fas fa-shopping-cart"></i></a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td scope="row"><a href="#" class="fw-bold">#3</a></td>
-                                            <td>Larry the Bird</td>
-                                            <td>¥299.99</td>
-                                            <td>300</td>
-                                            <td>
-                                                <a href="#" class="btn btn-md rounded font-sm"><i
-                                                        class="fas fa-shopping-cart"></i></a>
-                                            </td>
-                                        </tr>
+                                            <tr>
+                                                <td scope="row"><a href="#" class="fw-bold">#1</a></td>
+                                                <td>Mark</td>
+                                                <td>¥299.99</td>
+                                                <td>700</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-md rounded font-sm"><i
+                                                            class="fas fa-shopping-cart"></i></a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td scope="row"><a href="#" class="fw-bold">#2</a></td>
+                                                <td>Jacob</td>
+                                                <td>¥299.99</td>
+                                                <td>400</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-md rounded font-sm"><i
+                                                            class="fas fa-shopping-cart"></i></a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td scope="row"><a href="#" class="fw-bold">#3</a></td>
+                                                <td>Larry the Bird</td>
+                                                <td>¥299.99</td>
+                                                <td>300</td>
+                                                <td>
+                                                    <a href="#" class="btn btn-md rounded font-sm"><i
+                                                            class="fas fa-shopping-cart"></i></a>
+                                                </td>
+                                            </tr>
                                         </tbody>
                                     </table>
 
@@ -131,18 +138,18 @@
                 </div>
             </div>
 
-            <div class="col-xxl-5">
+            <!-- Checkout Part 2 -->
+            <div class="col-xxl-6">
                 <div class="card bg-warning-subtle">
                     <div class="card-body">
                         <div class="row">
-                            <!-- Checkout Part 2 -->
                             <div class="col-xxl-12 col-xl-12 col-sm-12">
                                 <header>
                                     <h4 class="d-flex align-items-center justify-content-center">
                                         <span>
                                             <i class="fas fa-shopping-cart"></i>
                                         </span>
-                                        <span class="ms-2">Your cart has {{$cartContent->count()}} item(s)</span>
+                                        <span class="ms-2">Your cart has {{ $cartContent->count() }} item(s)</span>
                                     </h4>
                                 </header>
                                 <!-- Cart Content -->
@@ -150,49 +157,49 @@
 
                                     <table class="table">
                                         <thead class="table-light">
-                                        <tr class="border-bottom text-center">
-                                            <th scope="col">#SN</th>
-                                            <th scope="col">Image</th>
-                                            <th scope="col">Item</th>
-                                            <th scope="col">Quantity</th>
-                                            <th scope="col">Total</th>
-                                            <th scope="col">Action</th>
-                                        </tr>
+                                            <tr class="border-bottom text-center">
+                                                <th scope="col">#SN</th>
+                                                <th scope="col">Image</th>
+                                                <th scope="col">Item</th>
+                                                <th scope="col">Quantity</th>
+                                                <th scope="col">Total</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
                                         </thead>
 
                                         <tbody class="text-center">
-                                        @foreach($cartContent as $cartItem)
-                                            <tr class="border-bottom">
-                                                <td scope="row">
-                                                    <a href="#" class="fw-bold">#{{ $loop->index + 1 }}</a>
-                                                </td>
-                                                <td>
-                                                    <img style="height:4rem; width:4rem; object-fit: contain;"
-                                                         src="{{ asset('/assets/imgs/car-parts/tier.jpg') }}"
-                                                         alt="{{ $cartItem->name }}"
-                                                         class="img-fluid">
-                                                </td>
-                                                <td class="text-start">
-                                                    <p>{{ $cartItem->name }}</p>
-                                                    <p>¥{{ $cartItem->price }}</p>
-                                                </td>
-                                                <td>
-                                                    <div class="input-group d-flex justify-content-center">
-                                                        <a href="#" class="btn btn-light btn-sm fw-bold">-</a>
-                                                        <input type="number" class="form-control text-center" value="{{ $cartItem->qty }}" style="width: 1rem;">
-                                                        <a href="#" class="btn btn-light btn-sm fw-bold">+</a>
-                                                    </div>
-                                                </td>
-                                                <td class="text-end">
-                                                    <p>¥{{ $cartItem->price }}</p>
-                                                </td>
-                                                <td>
-                                                    <a href="#" class="btn btn-instagram rounded font-sm">
-                                                        <i class="fas fa-trash"></i>
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                            @foreach ($cartContent as $cartItem)
+                                                <tr class="border-bottom">
+                                                    <td scope="row">
+                                                        <a href="#" class="fw-bold">#{{ $loop->index + 1 }}</a>
+                                                    </td>
+                                                    <td>
+                                                        <img style="height:4rem; width:4rem; object-fit: contain;"
+                                                            src="{{ asset('/assets/imgs/car-parts/tier.jpg') }}"
+                                                            alt="{{ $cartItem->name }}" class="img-fluid">
+                                                    </td>
+                                                    <td class="text-start">
+                                                        <p>{{ $cartItem->name }}</p>
+                                                        <p>¥{{ $cartItem->price }}</p>
+                                                    </td>
+                                                    <td>
+                                                        <div class="input-group d-flex justify-content-center">
+                                                            <a href="#" class="btn btn-light btn-sm fw-bold">-</a>
+                                                            <input type="number" class="form-control text-center"
+                                                                value="{{ $cartItem->qty }}" style="width: 1rem;">
+                                                            <a href="#" class="btn btn-light btn-sm fw-bold">+</a>
+                                                        </div>
+                                                    </td>
+                                                    <td class="text-end">
+                                                        <p>¥{{ $cartItem->price }}</p>
+                                                    </td>
+                                                    <td>
+                                                        <a href="#" class="btn btn-instagram rounded font-sm">
+                                                            <i class="fas fa-trash"></i>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
                                         </tbody>
                                     </table>
                                 </div>
